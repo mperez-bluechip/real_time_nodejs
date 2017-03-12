d5c57ebb82e308da408295f039352e5e23196b79
Manny Perez <threedyou@gmail.com>
2017-03-11 18:01:49 -0600

    Added 9.server.js and 9.html demonstrating web socket io to other   users with socket emit and broadcasting to achieve realtime messaging   across multiple open instances of the same page anywhere

---
+++ 9.server.js
@@ -0,0 +1,57 @@
+function handleHTTP(req,res) {
+	if (req.method == "GET") {
+		if (/^\/\d+(?=$|[\/?#])/.test(req.url)) {
+			req.addListener("end",function(){
+				req.url = req.url.replace(/^\/(\d+).*$/,"/$1.html");
+				static_files.serve(req,res);
+			});
+			req.resume();
+		}
+		else {
+			res.writeHead(403);
+			res.end();
+		}
+	}
+	else {
+		res.writeHead(403);
+		res.end();
+	}
+}
+
+
+var
+	http = require("http"),
+	httpserv = http.createServer(handleHTTP),
+
+	port = 8006,
+	host = "127.0.0.1",
+
+	ASQ = require("asynquence"),
+	node_static = require("node-static"),
+	static_files = new node_static.Server(__dirname),
+
+	io = require("socket.io").listen(httpserv)
+;
+
+require("asynquence-contrib");
+
+httpserv.listen(port, host);
+
+
+
+io.on('connection', function(socket) {
+		clearInterval(intv);
+    console.log('Client connected.');
+
+    socket.on('disconnect', function() {
+        console.log('Client disconnected.');
+    });
+
+		socket.on("typeit",function(msg){
+			socket.broadcast.emit("messages", msg);
+		});
+
+		var intv = setInterval(function(){
+			socket.emit("hello", Math.random());
+		},1000);
+});
